02cd646a719af4ec0e7ebccbbe7c14e93e3a4565
Kirill Vorobiov <mishkacod@gmail.com>
2017-09-21 17:47:57 +0300

    add semantic ui and remove/add button

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,6 +4,8 @@ import CarsApp from './app/CarsApp';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import styled from 'styled-components';
+// import '../../node_modules/semantic-ui-css/semantic.min.css';
+
 
 ReactDOM.render(
 	<CarsApp />,
