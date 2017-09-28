8e7f25262fe779a273136278d0c9e48ab3b1cd5b
Kirill Vorobiov <mishkacod@gmail.com>
2017-09-28 17:26:27 +0300

    complete logic of ADD / DELL Items in Cart, add lodash, dell todos

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,13 +1,14 @@
 import '../styles/main.sass';
 
-import CarsApp from './app/CarsApp';
+import MyCarStore from './app/App';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import styled from 'styled-components';
+
 // import '../../node_modules/semantic-ui-css/semantic.min.css';
 
 
 ReactDOM.render(
-	<CarsApp />,
+	<MyCarStore />,
 	document.getElementById('root'),
 );
